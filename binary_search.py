8d383084bae4fafe801c63ae09da721cd8d64e83
rosenam <austin.rosen1@gmail.com>
2023-01-26 08:41:52 -0800

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,64 +1,59 @@
-# -*- coding: utf-8 -*-
 """
 Austin Rosen
 Binary Search Algorithm
 """
+
+
 ##################################
-def main(n,m,k,l):
-    
+def main(n, m, k, l):
     '''
     This function makes calls to the binary search algorithm,
     and writes the output to a file.
     '''
-    
+
     out_handle = open("binary_search_results.txt", "w")
     for num in l:
-        binary(k,k,num,out_handle)
+        binary(k, num, out_handle)
     return
 
+
 ##################################
-    
-def binary(k,tmp,num,out_handle):
-    
-    '''
+
+def binary(k, num, out_handle):
+    """
     This function performs the binary search, with recursive calls to the function
-    '''
-    
-    n = len(tmp)            # find length of array
-    half = int(n/2)         # find index for the midpoint of the array
-    if n == 0:              # if array is empty, answer is -1
-        print(-1)
-        out_handle.write("-1 ")
-        return
-    elif n == 1:            # if array has a single value, check if key equals array value
-        if num == tmp[0]:
-            print(k.index(num) + 1)
-            out_handle.write(str(k.index(num) + 1) + " ")
-            return
+    """
+
+    min = 0
+    max = len(k)-1
+    mid = 0
+
+    while min <= max:
+
+        mid = (min + max) // 2
+
+        if int(num) < int(k[mid]):
+            max = mid - 1
+
+        elif int(num) > int(k[mid]):
+            min = mid + 1
         else:
-            print(-1)
-            out_handle.write("-1 ")
-            return 
-    else:                   # check if the key is greater than, less than, or equal to the midpoint value of the array
-        if num == tmp[half]:            # if equal, return array index
-            print(k.index(num) + 1)
-            out_handle.write(str(k.index(num) + 1) + " ")
+            mid = mid + 1
+            print(mid)
+            out_handle.write(str(mid) + " ")
             return
-        elif num < tmp[half]:           # if less than, repeat algorithm
-            new_tmp = tmp[:half]        
-            binary(k, new_tmp, num, out_handle)
-        elif num > tmp[half]:           # if greater than, repeat algorithm
-            new_tmp = tmp[half+1:]
-            binary(k, new_tmp, num, out_handle)
-      
-####################################     
-            
+    print("-1")
+    out_handle.write("-1 ")
+    return
+
+####################################
+
 if __name__ == "__main__":
-    
-    in_handle = open("rosalind_bins_test.txt")      # read in data
-    
-    with in_handle:             # turn data into proper format for the main() function
-        data = [0,0,0,0]
+
+    in_handle = open("rosalind_bins.txt")  # read in data
+
+    with in_handle:  # turn data into proper format for the main() function
+        data = [0, 0, 0, 0]
         i = 0
         for line in in_handle:
             data[i] = line.rstrip()
@@ -67,7 +62,7 @@ if __name__ == "__main__":
         m = data[1]
         k = data[2].split()
         l = data[3].split()
-        
-        main(n,m,k,l)
+
+        main(n, m, k, l)
 
 ###########################################
